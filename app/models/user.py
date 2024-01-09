08d0db8e61a0cc54a84c3dcd4933b9efae4aa784
marcossuba <marcossuba@gmail.com>
2024-01-09 01:28:53 -0300

    conf arquivo yml

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+    # Métodos adicionais como para verificar senha, etc.
