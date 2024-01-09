3ed97506af8b72c23a52496d4c1ed6fc442c8b13
marcossuba <marcossuba@gmail.com>
2024-01-08 21:25:43 -0300

    inclusão de testes

--- app/views/main.py
+++ app/views/main.py
@@ -5,3 +5,7 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def home():
     return "Welcome to the QA Site Backend!"
+
+@main_bp.route('/about')
+def about():
+    return "Aqui vai alguma informação sobre o site ou aplicativo."
